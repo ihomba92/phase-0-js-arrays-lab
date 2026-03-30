6a74e60c2d6a65e4d538ba03f20b43c7ce4c30d1
ihomba92 <ihombakibandidavid@gmail.com>
2026-03-30 23:28:19 +0300

    solved the arrays functions

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,5 +1,36 @@
 // Write your code here
+// Creating the array called products
+const products = ['Laptop', 'Phone', 'Headphones', 'Monitor'];
 
+//console.log(products);
+//Accessing the product information
+function logFirstProduct(firstProduct){
+  console.log(products[0]);
+}
+//console.log(products[0]);
+
+//Adding a product
+function addProduct(product){
+ products.push(product);
+}
+ //addProduct('CPU');
+ console.log(addProduct('CPU'));
+
+ //Update product Information
+ function updateProductName(index, nameChange){
+  products[1]= nameChange;
+
+ }
+ //let namChange = "Smartphone";
+console.log (updateProductName(products[1], 'Smartphone'));
+
+//Remove a Product
+function removeLastProduct(nameremove){
+  products.pop();
+}
+removeLastProduct(4);
+
+ console.log(products);
 
 
 // Export the necessary parts for testing
@@ -10,3 +41,4 @@ module.exports = {
   removeLastProduct: typeof removeLastProduct !== 'undefined' ? removeLastProduct : undefined,
   products
 };
+
